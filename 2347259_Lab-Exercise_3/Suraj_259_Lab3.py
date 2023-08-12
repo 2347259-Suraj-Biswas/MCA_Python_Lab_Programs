ef3eedfd2dff1e87f5bcbe3c312be7d6c8d2c77b
2347259-Suraj-Biswas <141404791+2347259-Suraj-Biswas@users.noreply.github.com>
2023-08-12 10:29:26 +0530

    Add files via upload

---
+++ 2347259_Lab-Exercise_3/Suraj_259_Lab3.py
@@ -0,0 +1,24 @@
+# 1. Write a function in Python with a string such that it accepts a parameter- “stringsplit”. 
+# This encoded string will contain your name, domain name and register number. 
+# You can separate the values in the string by any number of underscores. 
+# [The string should not contain any other underscore symbols in your name, domain name and register number]. 
+# The function should return a Python dictionary with your name, domain name and register number._
+# For example, if the input would be " Aaron___Googleplaystore____2347201”. Then the function should return the output as follows:
+# { "name": " Aaron ",
+# "Domain_name": " Googleplaystore ",
+# "Regno": "2347201" }
+
+
+def string_name(stringsplit):
+    split_value = stringsplit.split('_')
+    stud_data = {
+        "name": split_value[0],
+        "Domain_name": split_value[1],
+        "Regno": split_value[2],
+    }
+    return stud_data
+
+my_data = input("Enter the data in the format name_Domain_name_Regno:")
+output = string_name(my_data)
+
+print(f"\n{output}\n")
